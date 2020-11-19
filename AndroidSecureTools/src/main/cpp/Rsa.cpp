57e10202b6913e23071522e0eb7319d2f342766d
Petr <petr-shubin70@mail.ru>
2020-11-20 01:06:59 +0600

    Версия 0.0.2.0 Новое: - Небольшие настройки CMakeLists.txt

---
+++ AndroidSecureTools/src/main/cpp/Rsa.cpp
@@ -0,0 +1,8 @@
+//
+// Created by AlphaN on 19.11.2020.
+//
+class Rsa {
+
+    void generateKeyPair(int keyLength) {
+    }
+};
---
+++ AndroidSecureTools/src/main/cpp/native-lib.cpp
@@ -0,0 +1,5 @@
+#include "iostream"
+#include "Rsa.cpp"
+
+class Main {
+};
